2e2db7b44fadc544fb5c10d1a37b861de762321f
Yaron Karasik <yaronkarasik@gmail.com>
2014-06-25 17:58:15 -0400

    Update skills and app id

--- attdemoapp/LIOMainViewController.m
+++ attdemoapp/LIOMainViewController.m
@@ -104,10 +104,10 @@
     // Do any additional setup after loading the view.
     
     self.accountSkills = [NSMutableArray array];
-    [self.accountSkills addObject:@{@"account" : @"10188673", @"skill" : @"dslcare13-mobile-ios-english"}];
     [self.accountSkills addObject:@{@"account" : @"1771134", @"skill" : @"service_olam-mobile-ios-english"}];
-    [self.accountSkills addObject:@{@"account" : @"1771134", @"skill" : @"wireless-mobile-ios-english"}];
     [self.accountSkills addObject:@{@"account" : @"10188673", @"skill" : @"dslcare9-mobile-ios-english"}];
+    [self.accountSkills addObject:@{@"account" : @"1771134", @"skill" : @"wireless-mobile-ios-english"}];
+    [self.accountSkills addObject:@{@"account" : @"10188673", @"skill" : @"dslcare13-mobile-ios-english"}];
     
     for (NSDictionary *accountSkill in self.accountSkills)
     {
